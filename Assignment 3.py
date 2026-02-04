1aa27db56025741d7dda663597cca719c94d680c
gardeharshada <gardeharshada74@gmail.com>
2026-02-04 19:34:42 +0530

    Update input prompt for vowel/consonant check

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,7 +1,8 @@
 # Write a program to input any alphabet and check whether it is vowel or consonant.
-ch = input("a")
+ch = input("Enter an alphabet:")
 
 if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
     print("The alphabet is a Vowel")
 else:
+
     print("The alphabet is a consonant")    
