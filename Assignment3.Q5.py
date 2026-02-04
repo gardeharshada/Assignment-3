28dc5d351e8153140b6fd28d46cb85169dbb8e84
gardeharshada <gardeharshada74@gmail.com>
2026-02-04 22:16:06 +0530

    Add files via upload

---
+++ Assignment3.Q5.py
@@ -0,0 +1,14 @@
+# Write a program to check whether the triangle is equilateral, isosceles or scalene triangle.
+a = float(input("Enter first side:"))
+b = float(input("Enter second side:"))
+c = float(input("Enter third side:"))
+
+if a + b > c and a + c > b and b + c > a:
+    if a == b == c:
+        print("The triangle is Equilateral")
+    elif a == b or b ==  c or a == c:
+        print("The triangle is Isosceles") 
+    else:
+        print("The triangle is Scalene") 
+else:     
+    print("The triangle is not valid")             
